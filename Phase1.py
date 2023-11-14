d19878602c33f36e80b953254b76b0017ee8290b
Marius Tamokoue <mariustamokoue@gmail.come>
2023-11-14 15:20:41 -0500

    ajout affichage liste et autres fonctions

--- Phase1.py
+++ Phase1.py
@@ -1,5 +1,5 @@
 import argparse
-from datetime import datetime
+from datetime import datetime, date
 import requests
 import json
 
@@ -16,9 +16,9 @@ def analyser_commande():
     parser = argparse.ArgumentParser('Extraction de valeurs historiques pour un ou plusieurs symboles boursiers.')
     
     parser.add_argument("symbols", nargs="+", help="Nom d'un symbole boursier")
-    parser.add_argument("-h", "--help", help = "show this help message and exit")
-    parser.add_argument("-d", "--date debut", help="Date recherchée la plus ancienne (format: AAAA-MM-JJ)")
-    parser.add_argument("-f", "--date fin", help="Date recherchée la plus récente (format: AAAA-MM-JJ)")
+    #parser.add_argument("-h", "--help", help = "show this help message and exit")
+    parser.add_argument("-d", "--date_debut", help="Date recherchée la plus ancienne (format: AAAA-MM-JJ)")
+    parser.add_argument("-f", "--date_fin", help="Date recherchée la plus récente (format: AAAA-MM-JJ)")
     parser.add_argument("-v", "--valeur", choices=["fermeture", "ouverture", "min", "max", "volume"], default="fermeture", help="Valeur désirée")
     # Complétez le code ici
     # vous pourriez aussi avoir à ajouter des arguments dans ArgumentParser(...)
@@ -27,22 +27,54 @@ def analyser_commande():
 
 
 def produire_historique(nom, debut, fin, valeur_desirée):
-    return (debut, valeur_desirée)   
+    #symbole = 'GOOG'
+    url = f'https://pax.ulaval.ca/action/{nom}/historique/'
 
- 
-symbole = 'HOG'
-url = f'https://pax.ulaval.ca/action/{symbole}/historique/'
+    params = {
+        'début': debut,
+        'fin': fin,
+    }
 
-params = {
-    'début': '2019-02-18',
-    'fin': '2019-02-24',
-}
+    réponse = requests.get(url=url, params=params)
 
-réponse = requests.get(url=url, params=params)
-
-réponse = json.loads(réponse.text)
-print(réponse)
-
-for clé in réponse.keys():
-    print(clé)
+    if réponse.status_code == 200:
+        réponse = json.loads(réponse.text)
+        #print(réponse)
+        affichage = []
+      
+        for valeur_date, valeurs in réponse['historique'].items():
+           
+            try:
+                date = datetime.strptime(valeur_date, '%Y-%m-%d').date()
+               
+            except ValueError:
+                continue
+            
+            valeur = valeurs.get(valeur_desirée)
+            
+            if valeur is not None:
+                affichage.append((date, valeur))
+                
+        #print (affichage)
+        return affichage
+    else:
+        print(f'Erreur lors de la requête: {réponse.status_code}')
+        return None
     
+def afficher_historique(nom, debut, fin, valeur_desirée, réponse):
+     print (f'titre={nom}: valeur={valeur_desirée}, début={debut}, fin={fin}')
+     print(réponse)
+        
+if __name__ == '__main__':
+    
+    args = analyser_commande()
+    
+    start = args.date_debut if args.date_debut else date.today().isoformat()
+    end = args.date_fin if args.date_fin else date.today().isoformat()
+    
+    for symbole in args.symbols:
+        historique = produire_historique(symbole,start, end, args.valeur)
+        if historique is not None:
+            afficher_historique(symbole, start, end, args.valeur, historique)
+        
+
