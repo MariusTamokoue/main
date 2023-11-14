15a5c068e11f0c1d4d4536c1b46a2d340732d913
Marius Tamokoue <mariustamokoue@gmail.come>
2023-11-14 16:06:08 -0500

    Amelioration au progarmme

--- phase1.py
+++ phase1.py
@@ -13,13 +13,16 @@ def analyser_commande():
         symboles à traiter, et les attributs «début», «fin» et «valeur»
         associés aux arguments optionnels de la ligne de commande.
     """
-    parser = argparse.ArgumentParser('Extraction de valeurs historiques pour un ou plusieurs symboles boursiers.')
-    
+    parser = argparse.ArgumentParser('Extraction de valeurs historiques
+                                      'pour un ou plusieurs symboles boursiers.')
     parser.add_argument("symbols", nargs="+", help="Nom d'un symbole boursier")
     #parser.add_argument("-h", "--help", help = "show this help message and exit")
-    parser.add_argument("-d", "--date_debut", help="Date recherchée la plus ancienne (format: AAAA-MM-JJ)")
-    parser.add_argument("-f", "--date_fin", help="Date recherchée la plus récente (format: AAAA-MM-JJ)")
-    parser.add_argument("-v", "--valeur", choices=["fermeture", "ouverture", "min", "max", "volume"], default="fermeture", help="Valeur désirée")
+    parser.add_argument("-d", "--date_debut", help="Date recherchée la plus ancienne 
+                        "(format: AAAA-MM-JJ)")
+    parser.add_argument("-f", "--date_fin", help="Date recherchée la plus récente 
+                        "(format: AAAA-MM-JJ)")
+    parser.add_argument("-v", "--valeur", choices=["fermeture", "ouverture", "min",
+                        "max", "volume"], default="fermeture", help="Valeur désirée")
     # Complétez le code ici
     # vous pourriez aussi avoir à ajouter des arguments dans ArgumentParser(...)
 
@@ -42,19 +45,15 @@ def produire_historique(nom, debut, fin, valeur_desirée):
         #print(réponse)
         affichage = []
       
-        for valeur_date, valeurs in réponse['historique'].items():
-           
+        for valeur_date, valeurs in réponse['historique'].items(): 
             try:
                 date = datetime.strptime(valeur_date, '%Y-%m-%d').date()
-               
             except ValueError:
-                continue
-            
+                continue  
             valeur = valeurs.get(valeur_desirée)
             
             if valeur is not None:
-                affichage.append((date, valeur))
-                
+                affichage.append((date, valeur))         
         #print (affichage)
         return affichage
     else:
@@ -62,13 +61,11 @@ def produire_historique(nom, debut, fin, valeur_desirée):
         return None
     
 def afficher_historique(nom, debut, fin, valeur_desirée, réponse):
-     print (f'titre={nom}: valeur={valeur_desirée}, début={debut}, fin={fin}')
-     print(réponse)
+    print (f'titre={nom}: valeur={valeur_desirée}, début={debut}, fin={fin}')
+    print(réponse)
         
 if __name__ == '__main__':
-    
     args = analyser_commande()
-    
     start = args.date_debut if args.date_debut else date.today().isoformat()
     end = args.date_fin if args.date_fin else date.today().isoformat()
     
@@ -76,5 +73,3 @@ if __name__ == '__main__':
         historique = produire_historique(symbole,start, end, args.valeur)
         if historique is not None:
             afficher_historique(symbole, start, end, args.valeur, historique)
-        
-
